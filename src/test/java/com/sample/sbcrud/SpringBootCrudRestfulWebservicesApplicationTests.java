1438f35e2721b15bc08ed9dd61f5ed5b85bfd428
Shireesha-Kunamaneni <88739303+Shireesha-Kunamaneni@users.noreply.github.com>
2021-08-12 18:04:04 +0530

    Add files via upload

---
+++ src/test/java/com/sample/sbcrud/SpringBootCrudRestfulWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.sbcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudRestfulWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
